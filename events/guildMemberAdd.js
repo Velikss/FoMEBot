31aa257ffca8816b3a28424fae4721685bda5312
Velikss <felixbeuving@live.nl>
2018-04-20 22:06:12 +0200

    logs displayName instead of user

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 exports.run = (client, member) => {
   member.guild.channels.find("name", "welcome").send("Welcome " + member.user + " to the Fellowships of Middle Earth discord guild!");
-  console.log(member.user + " joined " + member.guild.name);
+  console.log(member.displayName + " joined " + member.guild.name);
 };
