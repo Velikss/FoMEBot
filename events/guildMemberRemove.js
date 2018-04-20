e5b111e96a818d7e10edf9b88048ab6ac94acfb0
Velikss <felixbeuving@live.nl>
2018-04-20 22:52:07 +0200

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
 exports.run = (client, member) => {
-  member.guild.channels.find("name", "welcome").send(member.user + ' just left the server! 🙁');
+  member.guild.channels.find("name", "welcome").send(member.username + ' just left the server! 🙁');
   console.log(member.displayName + " left " + member.guild.name);
 };
