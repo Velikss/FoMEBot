39d1909b2d9cb2ecd7c212eafd3a662730c13d48
Velikss <felixbeuving@live.nl>
2018-04-20 20:34:58 +0200

    edit to guildMemberAdd

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+exports.run = (client, member) => {
+  const WelcomeChannel = member.guild.channels.find("id", "336852719950364674");
+  defaultChannel.send('Welcome ' + member.user + ' to the Fellowships of Middle Earth discord guild!');
+  console.log(member.user + ' joined ' member.guild.name);
+};
