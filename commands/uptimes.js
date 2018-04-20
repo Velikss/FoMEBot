521ca75e528b195c63d4d10440449283cd4b5a5e
Yarden Akin <yardenakin@gmail.com>
2018-04-20 07:11:14 +0000

    Added config.json.example

--- commands/uptimes.js
+++ commands/uptimes.js
@@ -1,10 +1,9 @@
 exports.run = (client, message, args) => {
-    if (message.member.roles.find("name", "Staf")) {
+    if (message.member.roles.find("name", "Staff")) {
         var minutes = client.uptime / 1000 / 60;
         message.channel.send('I have been running for: ' + minutes + ' minutes.');
     } else {
         message.channel.send('Sadly this command is not available to you :slight_frown:');
-        message.react(":ok:");
     }
 };
 
